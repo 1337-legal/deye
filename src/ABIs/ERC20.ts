7d514cb42110ea7bba04daea5b0f61f39055c60d
alexn78412 <alex.noe@gmail.com>
2025-01-02 02:16:04 +0100

    initialize project with bun, add README, package.json, TypeScript configuration, and ERC20 ABI

---
+++ src/ABIs/ERC20.ts
@@ -0,0 +1,20 @@
+export default [
+	{
+		constant: true,
+		inputs: [],
+		name: "name",
+		outputs: [{ name: "", type: "string" }],
+		payable: false,
+		stateMutability: "view",
+		type: "function",
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: "symbol",
+		outputs: [{ name: "", type: "string" }],
+		payable: false,
+		stateMutability: "view",
+		type: "function",
+	},
+];
